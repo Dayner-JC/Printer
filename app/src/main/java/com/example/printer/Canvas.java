9711b55279b9e2c0c4c1636e0a361976edd3ba70
daynerjc <daynercespedes4@gmail.com>
2024-07-19 10:19:55 -0400

    feat: Add functionality to select and print bitmap imagesThis commit introduces a new feature that allows users to select a bitmap image from their gallery and send it to the printer.

--- app/src/main/java/com/example/printer/Canvas.java
+++ app/src/main/java/com/example/printer/Canvas.java
@@ -1,14 +1,20 @@
 package com.example.printer;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class Canvas extends AppCompatActivity {
+    private static final int REQUEST_CODE_PICK_IMAGE = 100;
     private CustomDrawingView drawingView;
     private PrinterHelper printerHelper;
 
@@ -21,6 +27,7 @@ public class Canvas extends AppCompatActivity {
         drawingView = findViewById(R.id.drawingView);
         Button btnPrintCanvas = findViewById(R.id.btnPrintCanvas);
         Button btnBack = findViewById(R.id.btnBack);
+        Button btnBitmap = findViewById(R.id.btnBitmap);
 
         printerHelper = new PrinterHelper();
         printerHelper.initSunmiPrinterService(this);
@@ -36,10 +43,31 @@ public class Canvas extends AppCompatActivity {
             finish();
         });
 
+        btnBitmap.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+        });
+
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         printerHelper.deInitSunmiPrinterService(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
+                printerHelper.printBitmap(bitmap, 0);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
